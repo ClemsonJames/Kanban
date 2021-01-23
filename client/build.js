67388a1c2a196967d0e6c4f23401b57780ad9825
qing lin <qingl114@hotmail.com>
2021-01-23 12:18:20 -0500

    csv export

--- client/build.js
+++ client/build.js
@@ -3661,7 +3661,8 @@ angular.module('app').
 
         userService.exportCSV = function(data) {
             var filename = 'cuhackit_users.csv'
-            var fields = ['info.name', 'email','info.major','info.sex','info.race'];
+            var fields = ['info.name', 'email','info.major', 'info.level','info.sex','info.race', 'info.address.street', 'info.address.street', 'info.address.apartNum', 
+                            'info.address.city', 'info.address.state', 'info.address.zip', 'country (US ONLY)'];
             var csv;
             try {
                 csv = json2csv(data, {fields});
